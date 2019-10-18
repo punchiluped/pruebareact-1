aec960c5f62bb6d4dde36d95fdcdab4d7220ee5f
JsVasquezAs <jsvasquezas@gmail.com>
2019-10-18 10:54:42 -0500

    Setting up our React app

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "primera-pp-uploads",
+      BUCKET: "South America (Sao Paulo)"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+      REGION: "us-east-2_WFjK7jyKx",
+      USER_POOL_ID: "arn:aws:cognito-idp:us-east-2:084836723973:userpool/us-east-2_WFjK7jyKx",
+      APP_CLIENT_ID: "35lt1b4m1j0eplj5r4ke830l2o",
+      IDENTITY_POOL_ID: "us-east-2:218a0ca6-7992-4fd7-a776-b981402b7aad"
+    }
+  };
